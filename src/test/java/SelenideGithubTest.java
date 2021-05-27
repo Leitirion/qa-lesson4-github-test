9d7d42b2251532b85f5b204b8d823f32302af347
Ilnar Gilyaev <gilyaev.if@gmail.com>
2021-05-28 00:46:34 +0500

    fix for test1 name, add HerokuDragAndDropTest

--- src/test/java/SelenideGithubTest.java
+++ src/test/java/SelenideGithubTest.java
@@ -1,4 +1,5 @@
 
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 
@@ -15,7 +16,7 @@ public class SelenideGithubTest {
 
     @Test
 
-     void AutomationFormTest() {
+     void GithubTest() {
         open("https://github.com/selenide/selenide"+"?q="+i);
         $x("//span[text()=\"Wiki\"]").click();
         $x("//input[@id=\"wiki-pages-filter\"]").setValue("SoftAssertions").pressEnter();
@@ -23,5 +24,16 @@ public class SelenideGithubTest {
         $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
 
     }
-    
+
+    @Test
+    void HerokuDragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $x("//div[@id=\"column-a\"]").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $x("//div[@id=\"column-a\"]").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+
 }

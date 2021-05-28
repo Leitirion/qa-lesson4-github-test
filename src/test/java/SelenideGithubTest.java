1ff6ef9447e287f34817d414a60ff03a0da8afbc
Ilnar Gilyaev <gilyaev.if@gmail.com>
2021-05-28 12:24:33 +0500

    add test for difference between (tr td) and (tr).(td)

--- src/test/java/SelenideGithubTest.java
+++ src/test/java/SelenideGithubTest.java
@@ -1,10 +1,10 @@
 
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -13,6 +13,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class SelenideGithubTest {
     Random randomGenerator = new Random();
     int i = randomGenerator.nextInt();
+
 
     @Test
 
@@ -35,5 +36,44 @@ public class SelenideGithubTest {
         $("#column-b").shouldHave(text("A"));
     }
 
+    @Test
 
+
+    void Test() {
+        String URL = "https://demoqa.com/automation-practice-form";
+        String firstName = "firstName";
+        String lastName = "lastName";
+        String StudentEmail = "userEmail@gmail.com";
+        String userNumber = "1234567890";
+
+        open(URL);
+
+        $("#firstName").setValue(firstName);
+
+        $("#lastName").setValue(lastName);
+
+        $("#userEmail").setValue(StudentEmail);
+        $("#userNumber").setValue(userNumber);
+        $x("//input[@id=\"gender-radio-2\"][@value=\"Female\"]").doubleClick();
+        $x("//input[@id=\"dateOfBirthInput\"]").click();
+        $x("//select[@class=\"react-datepicker__year-select\"]//option[@value=\"2001\"][text()=\"2001\"]").click();
+        $x("//select[@class=\"react-datepicker__month-select\"]//option[@value=\"3\"][text()=\"April\"]").click();
+        $x("//div[@class=\"react-datepicker__month\"]//div[text()=1]").click();
+
+        $("#subjectsInput").setValue("History").pressEnter();
+        $("[for='hobbies-checkbox-3']").click();
+        $("#currentAddress").setValue("Russia, Ufa.");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Noida").pressEnter();
+        $("#submit").pressEnter();
+
+        //        Вместо $("h1 div"); и $("h1").$("div") использовал $("tr td") и $("tr").$("td")
+        //      $("tr td") ищет все tr в которых есть td, выбирает из них первый tr td
+        //        $("tr").$("td") ищет все tr, выбирает первый из них, в нем ищет td и выбирает первый, если такого нет, то тест падает
+
+        $("tr td").should(exist);
+        $("tr").$("td").should(exist);
+       // Element not found {tr/td}
+
+    }
 }
